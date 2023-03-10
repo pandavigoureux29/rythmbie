410759325ac5b497738730465cfe6941295f71c3
Vincent Roumier <vincent.roumier@ubisoft.com>
2023-03-10 15:17:15 -0500

    shotgun light firing

--- Assets/Scripts/gameplay/Visuals/WeaponVisual.cs
+++ Assets/Scripts/gameplay/Visuals/WeaponVisual.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,26 @@ public class WeaponVisual : MonoBehaviour
 {
     [SerializeField] private ParticleSystem m_fireArmEffect;
     [SerializeField] private AudioSource m_audio;
+    [SerializeField] private GameObject m_lightPrefab;
+    [SerializeField] private Transform m_lightAnchor;
+
+    private Animator m_lightAnimator;
+
+    private void Awake()
+    {
+        var go = Instantiate(m_lightPrefab);
+        m_lightAnimator = go.GetComponent<Animator>();
+    }
 
     public void TriggerEffect()
     {
         m_fireArmEffect.Play();
         m_audio.Play();
+        if (m_lightAnimator != null)
+        {
+            m_lightAnimator.transform.position = m_lightAnchor.position;
+            m_lightAnimator.transform.rotation = m_lightAnchor.rotation;
+            m_lightAnimator.SetTrigger("Fire");
+        }
     }
 }
